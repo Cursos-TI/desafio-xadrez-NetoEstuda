7c8c8a5bada03955660c7c00dddb7d54854abfba
NetoEstuda <silenceneto@gmail.com>
2025-08-08 20:58:16 +0000

    Acrescimo do cavalo

--- xadrez.c
+++ xadrez.c
@@ -9,6 +9,7 @@ int main() {
     int t = 0; // Variavel para a movimentação da Torre
     int b = 0; // Variavel para movimentação do Bispo
     int r = 0; // Varíavel para movimentação da Raínha
+    int c = 0; // Variavel para movimentação do Cavalo
     //Movimentação da Torre:
     printf("Movimento da Torre\n");
     for (t = 0; t < 5; t++){
@@ -28,6 +29,16 @@ int main() {
         printf("Esquerda\n");
         r++;
     } while (r < 8);
+
+    printf("Movimento do Cavalo\n");
+
+    for (c = 0; c < 1; c++){
+        printf("Direita\n");
+        for (c = 0; c < 2; c++){
+            printf("Baixo\n");
+        }
+    }
+
 
 
     return 0;
