1aa1943d6f5857720d59d2adc5cc5a0d764c52c7
NetoEstuda <silenceneto@gmail.com>
2025-08-13 15:07:29 +0000

    Nível Mestre. Acrescentado recursividade e loops

--- xadrez.c
+++ xadrez.c
@@ -4,41 +4,56 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+void moverTorre(int mover){ //função para movimentação da torre
+
+    if (mover == 0) return; //IF necessário para encerrar a recursão. Quando "mover" chegar a 0, a função para.
+    printf("Direita\n"); // Cada vez que a função é chamada, a peça anda uma casa na respectiva direção
+    moverTorre(mover-1); // Aqui eu chamo a função e subtraio até chegar a 0
+}
     
-    int t = 0; // Variavel para a movimentação da Torre
-    int b = 0; // Variavel para movimentação do Bispo
-    int r = 0; // Varíavel para movimentação da Raínha
-    int c = 0; // Variavel para movimentação do Cavalo
-    //Movimentação da Torre:
-    printf("Movimento da Torre\n");
-    for (t = 0; t < 5; t++){
-        printf("Direita\n");
-    }
+void moverRainha(int mover){
 
-    //Movimentação do Bispo
-    printf("Movimento do Bispo\n");
-    while (b < 5){
-        printf("Cima, Direita\n");
-        b++;
-    }
+    if (mover == 0) return;//IF necessário para encerrar a recursão. Quando "mover" chegar a 0, a função para.
+    printf("Esquerda\n"); // Cada vez que a função é chamada, a peça anda uma casa na respectiva direção
+    moverRainha(mover-1);// Aqui eu chamo a função e subtraio até chegar a 0
+}   
 
-    //Movimentação da Rainha
-    printf("Movimento da Rainha\n");
-    do {
-        printf("Esquerda\n");
-        r++;
-    } while (r < 8);
-
-    printf("Movimento do Cavalo\n");
-
-    for (c = 0; c < 1; c++){
-        printf("Direita\n");
-        for (c = 0; c < 2; c++){
-            printf("Baixo\n");
+void moverCavalo(){
+    
+    for (int i = 2; i > 0; i--){ //Condição para rodar duas vezes o movimento
+        printf("Cima\n");
+        for (int j = 1; j > 0; j--){ // Condição para rodar uma vez somente ao final do movimento anterior
+            if(i==1){
+                printf("Direita\n");
+            }
         }
     }
+}   
 
+void moverBispo(int mover){
+    
+    if(mover == 0) return;//IF necessário para encerrar a recursão. Quando "mover" chegar a 0, a função para.
+    for (int i = mover; i > mover - 1; i--){
+        printf("Cima\n");// Cada vez que a função é chamada, entra nesse loop e a peça anda uma casa na respectiva direção
+        for (int j = i; j > i-1; j--){
+            printf("Direita\n");// Cada vez que entra no loop externo, entra nesse loop interno e a peça anda uma casa na respectiva direção
+        }
+    }
+    moverBispo(mover-1);// Aqui eu chamo a função e subtraio até chegar a 0
+}   
+
+
+int main() {
+    
+    int mover = 5; //Uma variável para facilitar o uso
+    printf("------------------------------------------------------\nMovimento da Torre!\n\n");
+    moverTorre(mover);
+    printf("------------------------------------------------------\nMovimento da Rainha!\n\n");
+    moverRainha(mover+3); // Aqui eu somo conforme a necessidade. Nesse caso a Raínha anda 8 casas.
+    printf("------------------------------------------------------\nMovimento do Cavalo!\n\n");
+    moverCavalo(); // Cavalo possui um movimento único, não precisa de recursão
+    printf("------------------------------------------------------\nMovimento do Bispo!\n\n");
+    moverBispo(mover);
 
 
     return 0;
